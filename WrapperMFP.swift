2f9d8c99fdca261ff850e16946bc9d88f445972b
Raja Rahul <rajarahul@Rajas-MacBook-Pro.local>
2019-01-16 11:18:10 +0530

    Adding Code

---
+++ WrapperMFP.swift
@@ -0,0 +1,18 @@
+//
+//  WrapperMFP.swift
+//  Pods-WrapperMFP_Tests
+//
+//  Created by Raja Rahul on 16/01/19.
+//
+
+import UIKit
+
+public class WrapperMFP: UIView {
+
+    public func connect() -> String {
+        return "Hello";
+    }
+    
+    
+
+}
